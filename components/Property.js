bb4b97777b619682cd4cdce7945bb7881f0acac1
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-20 16:29:00 -0800

    progress on Property card component

--- components/Property.js
+++ components/Property.js
@@ -6,6 +6,8 @@ import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 
+import defaultImage from "../assets/house.jpg";
+
 const Property = ({
   property: {
     coverPhoto,
@@ -22,7 +24,52 @@ const Property = ({
 }) => {
   return (
     <Link href={`/property/${externalId}`} passHref>
-      {title}
+      <Flex
+        flexWrap="wrap"
+        w="420px"
+        p="5"
+        paddingTop="0"
+        justifyContent="flex-start"
+        cursor="pointer"
+      >
+        <Box>
+          <Image
+            src={coverPhoto ? coverPhoto.url : defaultImage}
+            alt="cover photo"
+            width={400}
+            height={260}
+          />
+        </Box>
+        <Box w="full">
+          <Flex paddingTop="2" alignItems="center" justifyContent="center">
+            <Flex alignItems="center">
+              <Box paddingRight="3" color="green.400">
+                {isVerified && <GoVerified />}
+              </Box>
+              <Text fontWeight="bold" fontSize="lg">
+                AED {millify(price)}
+                {rentFrequency && `/${rentFrequency}`}
+              </Text>
+            </Flex>
+            <Box>
+              <Avatar size="sm" src={agency?.logo?.url} />
+            </Box>
+          </Flex>
+          <Flex
+            alignItems="center"
+            p="1"
+            justifyContent="space-between"
+            w="250px"
+            color="blue.400"
+          >
+            {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+            <BsGridFill />
+          </Flex>
+          <Text fontSize="lg">
+            {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+          </Text>
+        </Box>
+      </Flex>
     </Link>
   );
 };
